d36c65f5a8d20f38ad2b22d276ab01a8883f483b
krsj2150 <kundansen1111@gmail.com>
2017-03-24 23:05:09 +0530

    Resizable windows in predefined templates added

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
 var body = window.frames.document.getElementByTagName("body")[0];
 var elem = window.frames.document.createElement("iframe");
 body.appendChild(elem);
-
--- options.js
+++ options.js
@@ -1,23 +1,23 @@
 var col = document.getElementById("col_plus");
-var row = document.getElementById("row_plus");
+//var row = document.getElementById("row_plus");
 
 var isFirst = 0;
 col.onclick = function() {
- 
-
   if( isFirst == 0 ){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.update(tabs[0].id, {url: "main.html"});
     });
     isFirst = 1;
-  }
-  else{
+  } else {
+    // add one more column
     //alert("second time");
   }
 };
 
+/*
 row.onclick = function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.update(tabs[0].id, {url: "main.html"});
     });
 };
+*/
