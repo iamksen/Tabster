cc7892d0e08434f12957e510912cbbae5fce2fa1
krsj2150 <kundansen1111@gmail.com>
2017-03-24 08:49:00 +0530

    Multitask Column added

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
 /*
 	background.js accepts all the chrome APIs
 */
+
+function addNewRow(response){
+	
+}
+
+chrome.runtime.onMessage.addListener( function(response, sender, sendresponse){
+	//alert(response);
+
+	chrome.tabs.update({url: response});
+	/*if( response == "column"){
+		chrome.tabs.update(sender.tab.id, {url: "main.html"});
+	} else {
+		alert("else alert");
+	}*/
+});
--- options.js
+++ options.js
@@ -2,7 +2,8 @@ var col = document.getElementById("col_plus");
 var row = document.getElementById("row_plus");
 
 col.addEventListener('click', function(){
-	alert("A column is added");
+	//alert("A column is added");
+	chrome.runtime.sendMessage("main.html");
 });
 
 row.addEventListener('click', function(){
