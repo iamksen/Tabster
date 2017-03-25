297c8d5f75d0da8a6a25838962a394c18eae90b6
krsj2150 <kundansen1111@gmail.com>
2017-03-25 13:14:41 +0530

    Directory structure changed

--- options.js
+++ options.js
@@ -6,24 +6,24 @@ var template_d = document.getElementById("template_d");
 
 template_a.onclick = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: "a.html"});
+    chrome.tabs.update(tabs[0].id, {url: "templates/a.html"});
   });
 };
 
 template_b.onclick = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: "b.html"});
+    chrome.tabs.update(tabs[0].id, {url: "templates/b.html"});
   });
 };
 
 template_c.onclick = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: "c.html"});
+    chrome.tabs.update(tabs[0].id, {url: "templates/c.html"});
   });
 };
 
 template_d.onclick = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: "d.html"});
+    chrome.tabs.update(tabs[0].id, {url: "templates/d.html"});
   });
 };
